5380166938d4eaa6749108a9fd3d6b4527779826
876pol <77174148+876pol@users.noreply.github.com>
2021-06-12 15:17:42 -0400

    delete Keys.java

--- src/main/java/com/pol/codebot/Keys.java
+++
@@ -1,9 +0,0 @@
-package com.pol.codebot;
-
-final public class Keys {
-    final private static String discord = "ODM1ODc4Mjg5MzU5NzY1NTE0.YIV2Ug.CbeU4kDlJO73oSqp5ZTt0JkvvwQ";
-
-    public static String getDiscord() {
-        return discord;
-    }
-}
